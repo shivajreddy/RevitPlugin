5be5ea97790efdc6943a1ded9d2d92faddd0fd55
Shiva TEClab <shivajreddy@outlook.com>
2024-04-29 09:18:06 -0400

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+namespace pilot
+{
+
+    public class Main : IExternalApplication
+    {
+        private static string _tabName;
+        private static string _panelName;
+        private static RibbonPanel _ribbonPanel;
+        public Result OnStartup(UIControlledApplication application)
+        {
+            _tabName = "pilot";
+            _panelName = "pilot";
+            application.CreateRibbonTab(_tabName);
+            _ribbonPanel = application.CreateRibbonPanel(_tabName, _panelName);
+
+            TestCommand.CreatePushButtonAndAddToPanel(_ribbonPanel);
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.NoCommandData)]
+    public class TestCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Test", "Command");
+            return Result.Succeeded;
+        }
+
+        public static void CreatePushButtonAndAddToPanel(RibbonPanel ribbonPanel)
+        {
+            var declaringType = MethodBase.GetCurrentMethod()?.DeclaringType;
+            if (declaringType == null) return;
+            var pushButtonName = declaringType?.Name;
+            const string pushButtonTextName = "Test Command";
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyLocation = assembly.Location;
+            const string iconName = "icon.png";
+            const string fullClassName = "pilot.TestCommand";
+            const string toolTipInfo = "Sample tool tip of this command";
+
+            var pushButtonData = new PushButtonData(
+                name: pushButtonName,
+                text: pushButtonTextName,
+                assemblyName: assemblyLocation,
+                className: fullClassName
+            )
+            {
+                ToolTip = toolTipInfo,
+                Image = ImageUtilities.LoadImage(assembly, iconName),
+                LargeImage = ImageUtilities.LoadImage(assembly, iconName),
+                ToolTipImage = ImageUtilities.LoadImage(assembly, iconName)
+            };
+            ribbonPanel.AddItem(pushButtonData);
+        }
+    }
+
+
+    public static class ImageUtilities
+    {
+        public static BitmapImage LoadImage(Assembly assembly, string name)
+        {
+            var img = new BitmapImage();
+            try
+            {
+                var resourceName = assembly.GetManifestResourceNames().FirstOrDefault(x => x.Contains(name));
+                var stream = assembly.GetManifestResourceStream(resourceName);
+                img.BeginInit();
+                img.StreamSource = stream;
+                img.EndInit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return img;
+        }
+    }
+
+
+
+}
---
+++ Readme.md
@@ -0,0 +1,51 @@
+﻿# Revit API project as a boiler plate
+
+What does this include
+- References
+	- RevitAPI.dll, RevitAPIUI.dll
+	- PresentationCore, System.XAML, WindowsBase
+
+
+
+How to duplicate this project.
+
+1. copy paste the root of this project folder
+
+2. In solution explorer, right click 'Solution', rename to new solution name
+
+3. In solution explorer, right click on project - 'C# pilot', rename to new project name
+
+4. Right click on project, select properties, go to 'Application' tab
+   - Change the 'Assembly Name' and 'Default Namespace'
+   - rename the namespace inside 'Main.cs' file
+
+5. references: revitapi.dll and revitapiui.dll, are copied properly
+But just recheck their location just in case
+
+6.1. Update the .addin file
+	- In solution explorer, rename the .addin file
+	Edit the .addin file
+	- set name
+	- set assembly, this is the location of compiled .dll file. Our Post-Build script should copy the built .dll file into this location
+	- fullclassname, this is the entry point of the application
+	- client id
+		- in VisualStudio press 'Tools' > 'Create GUID'
+		- make a new guid using python: import uuid, uuid.uuid4()
+6.2. Edit the 'Build Event', right click on the project, properties, 'Build Events' tab, click on 'Edit Post-build Event'
+	 The placeholder values used here is <pilot>. so say your project is `troy`, they you would remove `<pilot>` with `troy` with out backticks.
+	- create a directory with the project name to save dll files in the root folder, here root means the folder that revit looks for .addin files
+	`if not exist` make sure to use the new name of the project
+	- `Xcopy` reaname the from .addin file, this says that this .addin file in the prject root is copied
+	- `Xcopy` rename the to folder
+	and in the 'Debug' tab of the project properties, make sure that proper revit version .exe is selected
+
+7. Make sure to use the correct `fullClassName` for the commands
+in the 'CreatePushButtonAndAddToPanel' method of each command
+
+8. adding new icons ie., .png images for commands
+	- you can add new .png to your project in any new directory also. but make sure to set the
+	'Build Action' property to 'Embedded Resource' of the newly added .png file 
+	- the 'ImageUtilities' class basically finds the embedded resources of given name, so this image
+	can be in any directory in the given project, as long as it's an embedded resource.
+
+
